ad28a943271e1ce855a07161809fbfd9cdb4a41b
Naveen <naveen.kaushik.21@mountblue.tech>
2024-09-18 22:10:42 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -13,6 +13,27 @@ app.use("/", (req, res) => {
   res.send("helloooo from homepage..");
 });
 
+app.get("/user", (req, res) => {
+  res.send({
+    firstName: "Naveen",
+    lastName: "Kaushik",
+  });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data send to DB Successfully!");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data Deleted Successfully");
+});
+
+
+// this will match all the HTTP Method API calls to /test
+app.use("/test", (req, res) => {
+  res.send("test data....");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
